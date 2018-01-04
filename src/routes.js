a7d3fb1c1ea5055efa89d7331b39e7e8025c9599
andres raeth <raeth@localhost.localdomain>
2018-01-04 13:20:53 -0400

    funcion generadora de cubo

--- src/routes.js
+++ src/routes.js
@@ -6,15 +6,48 @@ const routes = Router();
 /**
  * GET home page
  */
-routes.get('/', (req, res) => {
 
-  fs.readFile('txt.json','utf8',function(err,data){
-    console.log(err,data,"s")
-    res.end(data);
-  })
+ const generate=(Ini,End)=>{
+
+  let M=Array()
+
+  let s={x:1,y:1,z:1,v:0}
+  let e={x:50,y:50,z:50}
+  let sa=Object.assign({},s)
+  let co=0
+  let se={x:3,y:3,z:6}
+
+  while(true){
+    M.push({x:sa.x,y:sa.y,z:sa.z})
+    if(sa.z<e.z){
+      sa.z+=1
+
+    }else if(sa.y<e.y){
+      sa=Object.assign(sa,{z:s.z})
+      sa.y+=1
+    }else if(sa.x<e.x){
+      sa=Object.assign(sa,{z:s.z})
+      sa=Object.assign(sa,{y:s.y})
+      sa.x+=1
+    }else{
+
+      break
+    }
+  }
+    r=M.findIndex((element)=>{if(element.x==se.x && element.y==se.y && element.z==se.z){return element}})
+    return M
+
+ }
+
+routes.get('/', (req, res) => {
+  res.render('index', { title });
   
 });
 
+routes.get('/generate', (req, res) => {
+  req.length.x
+  cont Cub=generate({1,1,1},{req.x,req.y,req.z})
+});
 /**
  * GET /list
  *
