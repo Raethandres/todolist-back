e64bbd1b9327b16eb75784f9fa93f76fe2276e12
andres raeth <raeth@localhost.localdomain>
2018-01-04 13:30:38 -0400

    rutas

--- src/routes.js
+++ src/routes.js
@@ -11,11 +11,9 @@ const routes = Router();
 
   let M=Array()
 
-  let s={x:1,y:1,z:1,v:0}
-  let e={x:50,y:50,z:50}
+  let s=Object.assign({},Ini)
+  let e=Object.assign({},End)
   let sa=Object.assign({},s)
-  let co=0
-  let se={x:3,y:3,z:6}
 
   while(true){
     M.push({x:sa.x,y:sa.y,z:sa.z})
@@ -34,7 +32,6 @@ const routes = Router();
       break
     }
   }
-    r=M.findIndex((element)=>{if(element.x==se.x && element.y==se.y && element.z==se.z){return element}})
     return M
 
  }
@@ -45,30 +42,18 @@ routes.get('/', (req, res) => {
 });
 
 routes.get('/generate', (req, res) => {
-  req.length.x
   cont Cub=generate({1,1,1},{req.x,req.y,req.z})
+  res.render('index', { title });
 });
-/**
- * GET /list
- *
- * This is a sample route demonstrating
- * a simple approach to error handling and testing
- * the global error handler. You most certainly want to
- * create different/better error handlers depending on
- * your use case.
- */
-routes.get('/list', (req, res, next) => {
-  const { title } = req.query;
 
-  if (title == null || title === '') {
-    // You probably want to set the response HTTP status to 400 Bad Request
-    // or 422 Unprocessable Entity instead of the default 500 of
-    // the global error handler (e.g check out https://github.com/kbariotis/throw.js).
-    // This is just for demo purposes.
-    next(new Error('The "title" parameter is required'));
-    return;
-  }
+routes.post('/update', (req, res) => {
+  r=Cub.findIndex((element)=>{if(element.x==req.x && element.y==req.y && element.z==req.z){return element}})
+  Cub[r]=Object.assign({},{v:req.v})
+  res.render('index', { title });
+});
 
+routes.get('/querry', (req, res) => {
+    r=M.findIndex((element)=>{if(element.x==se.x && element.y==se.y && element.z==se.z){return element}})
   res.render('index', { title });
 });
 
