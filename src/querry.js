a5f9cad55787a741455447a295e55a4d9c1be7ca
andres raeth <raeth@localhost.localdomain>
2018-01-25 10:38:40 -0400

    make

--- src/querry.js
+++ src/querry.js
@@ -1,12 +1,12 @@
 import { Pool } from 'pg'
 
 function conection(){
-	 pool = new Pool({connectionString:'postgres://postgres:kara@localhost:5432/todo'})
+	 pool = new Pool({connectionString:'postgres://mbdfzusjvgjvga:14771ec76fe86ac3f185bfde1e93e484b2c402b2499533007d0a7515b6456e88@ec2-54-235-220-220.compute-1.amazonaws.com:5432/d848clemua0f07'})
 }
 
 export function Select(value) {
-	const pool = new Pool({connectionString:'postgres://postgres:kara@localhost:5432/todo'})
-		console.log(value)
+	const pool = new Pool({connectionString:'postgres://mbdfzusjvgjvga:14771ec76fe86ac3f185bfde1e93e484b2c402b2499533007d0a7515b6456e88@ec2-54-235-220-220.compute-1.amazonaws.com:5432/d848clemua0f07'})
+		console.log(pool)
 		if(value.where){
 			var query = {
   			text: 'SELECT * FROM '+value.from+" WHERE id_li= $1",
@@ -22,7 +22,7 @@ export function Select(value) {
    return pool.query(query)
 }
 export function Insert(value){
-	const pool = new Pool({connectionString:'postgres://postgres:kara@localhost:5432/todo'})
+	const pool = new Pool({connectionString:'postgres://mbdfzusjvgjvga:14771ec76fe86ac3f185bfde1e93e484b2c402b2499533007d0a7515b6456e88@ec2-54-235-220-220.compute-1.amazonaws.com:5432/d848clemua0f07'})
 			var query = {
   			text: 'INSERT INTO '+value.from+" "+value.val+' VALUES' +value.vals+' RETURNING *',
   			values:[...value.values],
@@ -33,7 +33,7 @@ export function Insert(value){
 }
 
 export function Delete(value){
-	const pool = new Pool({connectionString:'postgres://postgres:kara@localhost:5432/todo'})
+	const pool = new Pool({connectionString:'postgres://mbdfzusjvgjvga:14771ec76fe86ac3f185bfde1e93e484b2c402b2499533007d0a7515b6456e88@ec2-54-235-220-220.compute-1.amazonaws.com:5432/d848clemua0f07'})
 			var query = {
   			text: 'DELETE FROM '+value.from+" WHERE "+value.id+"= $1",
   			values:[value.values]
@@ -43,7 +43,7 @@ export function Delete(value){
 
 }
 export function Update(value){
-	const pool = new Pool({connectionString:'postgres://postgres:kara@localhost:5432/todo'})
+	const pool = new Pool({connectionString:'postgres://mbdfzusjvgjvga:14771ec76fe86ac3f185bfde1e93e484b2c402b2499533007d0a7515b6456e88@ec2-54-235-220-220.compute-1.amazonaws.com:5432/d848clemua0f07'})
 		console.log(value)
 		if(value.where){
 			var query = {
